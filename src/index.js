8ed4e591b8247fb170721cfbfef6836aa2e6fc4f
lebedevichAV <lebedevichav@gmail.com>
2022-11-28 15:14:37 +0300

    feat: added class assignment to navigation links

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import "normalize.css";
 import "@styles";
+
+let nav_links = document.querySelectorAll(".navigation__link");
+
+for (let nav_link of nav_links) {
+  nav_link.addEventListener("mouseover", () => {
+    nav_link.classList.toggle("navigation__link_active");
+  });
+  nav_link.addEventListener("mouseout", () => {
+    nav_link.classList.toggle("navigation__link_active");
+  });
+}
